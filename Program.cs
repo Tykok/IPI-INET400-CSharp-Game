ee0533c7b5bee4ee0eb68791f18969666d4e2730
fscript98 <francisco.agudo-antonio@agileo-solutions.com>
2023-02-13 16:21:43 +0100

    mode duel et mode battle royal

--- Program.cs
+++ Program.cs
@@ -7,53 +7,64 @@ namespace Main
     {
         static void Main(string[] args)
         {
-            
-            // Instanciate two list of characters
-            List<Character> TeamA = new List<Character>() { new Berserker(), new Vampire(), new Zombie(), new Gardien(), new Guerrier() };
-            List<Character> TeamB = new List<Character>() { new Goule(), new Kamikaze(), new Liche(), new Pretre(), new Robot() };
-            List<Character> CopyTeamA = new List<Character>();
-            
-            // Make initiave Jet
-            var JetInitiativeA = TeamA[UtilsCharacters.getRandomIndex(TeamA)].Jet();
-            var JetInitiativeB = TeamB[UtilsCharacters.getRandomIndex(TeamB)].Jet();
-            
-            if(JetInitiativeA > JetInitiativeB) {
-                CopyTeamA = TeamA;
-                TeamA = TeamB;
-                TeamB = CopyTeamA;
-            }
-            
-            var round = 0;
-            do
+            Console.WriteLine("Veuillez sélectionner un mode de jeu...");
+            Console.WriteLine("Duel(tapez d) | Battle Royal (tapez b)");
+            string mode = new string(Console.ReadLine());
+
+            if (mode == "d")
             {
-                // Team A attack
-                foreach (var character in TeamA)
-                {
-                    // TeamA attack random character in teamB
-                    var randomIndex = UtilsCharacters.getRandomIndex(TeamA);
-                    character.AttackCharacter(TeamB[randomIndex]);
-                }
-
-                // Team B attack
-                foreach (var character in TeamB)
-                {
-                    // TeamB attack random character in TeamB
-                    var randomIndex = UtilsCharacters.getRandomIndex(TeamB);
-                    character.AttackCharacter(TeamA[randomIndex]);
-                }
-                round++;
-            } while(UtilsCharacters.checkSomebodyAlive(TeamA) || UtilsCharacters.checkSomebodyAlive(TeamB));
-
-
-            if (UtilsCharacters.checkSomebodyAlive(TeamA))
-            {
-                Console.WriteLine("Team A win");
+                Console.WriteLine("========== DUEL ==========");
             }
             else
             {
-                Console.WriteLine("Team B win");
-            }
+                Console.WriteLine("========== BATTLE ROYAL ==========");
+                // Instanciate two list of characters
+                List<Character> TeamA = new List<Character>() { new Berserker(), new Vampire(), new Zombie(), new Gardien(), new Guerrier() };
+                List<Character> TeamB = new List<Character>() { new Goule(), new Kamikaze(), new Liche(), new Pretre(), new Robot() };
+                List<Character> CopyTeamA = new List<Character>();
             
+                // Make initiave Jet
+                var JetInitiativeA = TeamA[UtilsCharacters.getRandomIndex(TeamA)].Jet();
+                var JetInitiativeB = TeamB[UtilsCharacters.getRandomIndex(TeamB)].Jet();
+            
+                if(JetInitiativeA > JetInitiativeB) {
+                    CopyTeamA = TeamA;
+                    TeamA = TeamB;
+                    TeamB = CopyTeamA;
+                }
+            
+                var round = 0;
+                do
+                {
+                    // Team A attack
+                    foreach (var character in TeamA)
+                    {
+                        // TeamA attack random character in teamB
+                        var randomIndex = UtilsCharacters.getRandomIndex(TeamA);
+                        character.AttackCharacter(TeamB[randomIndex]);
+                    }
+
+                    // Team B attack
+                    foreach (var character in TeamB)
+                    {
+                        // TeamB attack random character in TeamB
+                        var randomIndex = UtilsCharacters.getRandomIndex(TeamB);
+                        character.AttackCharacter(TeamA[randomIndex]);
+                    }
+                    round++;
+                } while(UtilsCharacters.checkSomebodyAlive(TeamA) || UtilsCharacters.checkSomebodyAlive(TeamB));
+
+
+                if (UtilsCharacters.checkSomebodyAlive(TeamA))
+                {
+                    Console.WriteLine("Team A win");
+                }
+                else
+                {
+                    Console.WriteLine("Team B win");
+                }
+            }
+
         }
     }
 }
