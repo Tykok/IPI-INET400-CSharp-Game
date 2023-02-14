49bbe960e6d5c628456f887b369af483575538f8
Tykok <treportelie12@gmail.com>
2023-02-14 14:07:08 +0100

    [Feat] Added the zombie specificity

--- src/characters/Zombie.cs
+++ src/characters/Zombie.cs
@@ -1,8 +1,18 @@
 namespace Characters;
+using Main.enumeration;
 
 public class Zombie : Character
 {
+    
+    public static bool AffectedByPain = false;
+    public static CharacterType CharacterType = CharacterType.UNHOLY;
+    
     public Zombie() : base(100, 0, 20, 60, 1000,  1, 1)
     {
+    }
+
+    public override int JetDefense()
+    {
+        return 0;
     }
 }
