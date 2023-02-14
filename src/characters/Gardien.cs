9144fddd1046cad012f8328ba2e38156176221f9
Najib HARRIBOU <najib.harribou.ext@orange.com>
2023-02-14 14:46:50 +0100

    Gardien override CounterDamage method

--- src/characters/Gardien.cs
+++ src/characters/Gardien.cs
@@ -1,8 +1,19 @@
+using Main.enumeration;
 namespace Characters;
 
 public class Gardien : Character
 {
+
+    public CharacterType CharacterType = CharacterType.SACRED;
+    public CharacterAttackType CharacterAttackType = CharacterAttackType.BLESSED;
     public Gardien() : base(50, 150, 50, 50, 150,  3, 3)
     {
+    }
+
+    protected override void CounterDamage(Character target, int attackMargin)
+    {
+        CurrentLife -= attackMargin * 2;
+        target.CurrentAttackNumber--;
+        CurrentAttackNumber--;
     }
 }
