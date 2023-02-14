23652a731c377fe55c30ebc3f3ac3905a0f5d995
Tykok <56304246+Tykok@users.noreply.github.com>
2023-02-14 14:31:34 +0100

    [Feat] Add fight sytem

--- src/characters/Goule.cs
+++ src/characters/Goule.cs
@@ -5,4 +5,13 @@ public class Goule : Character
     public Goule() : base(50, 80, 120, 30, 250,  5, 5)
     {
     }
+
+    /// <summary>
+    /// Used to heal the "Goule" if someone dead
+    /// </summary>
+    public void EatDeadCharacter()
+    {
+        var heal = new Random().Next(49, 101);
+        CurrentLife = CurrentLife + heal > MaximumLife ? MaximumLife : CurrentLife + heal;
+    }
 }
